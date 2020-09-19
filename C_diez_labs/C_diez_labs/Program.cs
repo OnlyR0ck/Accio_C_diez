79603e9fddb78b6bafd82695c999c20d2825e5dc
OnlyR0ck <guitarilu2019@gmail.com>
2020-09-19 16:10:24 +0300

    Try to add quess the num

--- C_diez_labs/C_diez_labs/Program.cs
+++ C_diez_labs/C_diez_labs/Program.cs
@@ -10,7 +10,7 @@ namespace C_diez_labs
             Console.WriteLine("1 - Factorial\n2 - Fibonnaci\n3 - Create shape\n4 - Quess the number game\n0 - exit");
             while (Convert.ToBoolean(code))
             {
-                code = Console.Read();
+                code = Convert.ToInt16(Console.ReadLine());
                 switch (code)
                 {
                     case 1:
@@ -36,7 +36,7 @@ namespace C_diez_labs
         static void Factorial()
         {
             Console.WriteLine("Enter a number : ");
-            int num = Convert.ToInt32(Console.Read());
+            int num = Console.Read();
             int result = 1;
             while (Convert.ToBoolean(num))
             {
@@ -59,7 +59,44 @@ namespace C_diez_labs
 
         static void QuessTheNumber()
         {
+            Console.Clear();
+            Console.WriteLine("YOU WANNA PLAY - LET'S PLAY. BY ALEX NEVSKIY");
+            Console.WriteLine("\n\nGuess the number from 0 to 1000");
+            int upper = 1000, lower = 0, middlle;
+            bool flag = true;
+            while (lower <= upper)
+            {
+                middlle = (upper + lower) / 2;
+                Console.WriteLine(String.Concat("Is the ", middlle, " your number? Type Y or N"));
+                switch (Console.ReadLine().ToLower())
+                {
+                    case "y":
+                        Console.WriteLine("Yeah, I'm won !!!");
+                        flag = false;
+                        break;
+                    case "n":
+                        Console.WriteLine("Ok, just another question.");
+                        break;
+                    default:
+                        Console.WriteLine("Are you kidding me? So, let's start again.");
+                        continue;
+                }
 
+                if (!flag) break;
+                Console.WriteLine(String.Concat("\nTell me, Is your num bigger or less than ", middlle, " Type B or L."));
+                switch (Console.ReadLine().ToLower())
+                {
+                    case "b":
+                        lower = middlle - 1;
+                        break;
+                    case "l":
+                        upper = middlle + 1;
+                        break;
+                    default:
+                        Console.WriteLine("Are you kidding me? So, let's start again.");
+                        continue;
+                }
+            }
         }
     }
 }
