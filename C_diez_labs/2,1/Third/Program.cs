d1166ef095f388056776b43883816fcf7ac0b419
OnlyR0ck <guitarilu2019@gmail.com>
2020-10-01 23:20:51 +0300

    Add menu to project

--- C_diez_labs/2,1/Third/Program.cs
+++ C_diez_labs/2,1/Third/Program.cs
@@ -8,5 +8,49 @@ namespace Third
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void menu()
+        {
+            int code;
+
+            while (true)
+            {
+                Console.WriteLine("1 - Counter.\n2 - Generic.\n3 - Ducks.\n0 - Exit.");
+                Console.Write("Answer: ");
+                int.TryParse(Console.ReadLine(), out code);
+                switch (code)
+                {
+                    case 1:
+                        first();
+                        break;
+                    case 2:
+                        second();
+                        break;
+                    case 3:
+                        third();
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        continue;
+                }
+
+            }
+        }
+        static void first()
+        {
+
+        }
+
+        static void second()
+        {
+
+        }
+
+        static void third()
+        {
+
+        }
     }
 }
