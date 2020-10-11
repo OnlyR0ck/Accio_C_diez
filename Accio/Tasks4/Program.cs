06de19e7020aad25acd741114ee497a2bffeb8c4
OnlyR0ck <guitarilu2019@gmail.com>
2020-10-11 15:38:47 +0300

    Add Task4.1

--- Accio/Tasks4/Program.cs
+++ Accio/Tasks4/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Net.Http.Headers;
-using System.Reflection.PortableExecutable;
-using System.Threading.Channels;
 
 namespace Tasks4
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var code = 1;
             while (Convert.ToBoolean(code))
@@ -18,33 +15,100 @@ namespace Tasks4
                 switch (code)
                 {
                     case 1:
+                        Drawing();
+                        Console.ReadKey(true);
                         break;
                     case 2:
+                        Console.ReadKey(true);
                         break;
                     case 3:
+                        Console.ReadKey(true);
                         break;
                     case 0:
                         Environment.Exit(0);
-                        break;
-                    default:
                         break;
                 }
 
                 Console.Clear();
             }
+
+            static void Drawing()
+            {
+                Console.Clear();
+                Shape[] shapes = new Shape[4];
+                var ShapeNum = 0;
+                
+                shapes[0] = new Triangle();
+                shapes[1] = new Cube();
+                shapes[2] = new Sphere();
+                GetShape(shapes, out ShapeNum).Draw();
+                ShapeNum++;
+
+                Console.WriteLine("\nWhat is this figure?\n1 - Triangle.\n2 - Cube.\n3 - Sphere.");
+                int code = 0;
+                Console.Write("\nYour choice: ");
+                int.TryParse(Console.ReadLine(), out code);
+                Console.WriteLine(ShapeNum == code ? "You Win!" : "You lose!");
+            }
+
+            static Shape GetShape(Shape[] shapes, out int ShapeNum)
+            {
+                ShapeNum = new Random().Next(3);
+                return shapes[ShapeNum];
+            }
         }
     }
 
-    abstract class Shape
+    internal abstract class Shape
     {
         public abstract void Draw();
     }
 
-    class Triangle : Shape
+    internal class Triangle : Shape
     {
         public override void Draw()
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < 10; i++)
+            {
+                for (var j = 0; j < i; j++) Console.Write('0');
+                Console.WriteLine();
+            }
+        }
+    }
+
+    internal class Cube : Shape
+    {
+        public override void Draw()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                for (var j = 0; j < 10; j++) Console.Write("0 ");
+                Console.WriteLine();
+            }
+        }
+    }
+
+    internal class Sphere : Shape
+    {
+        public override void Draw()
+        {
+            var r = 7.0;
+            var r_in = r - 0.4;
+            var r_out = r + 0.4;
+
+            for (var y = r; y >= -r; --y)
+            {
+                for (var x = -r; x < r_out; x += 0.5)
+                {
+                    var value = x * x + y * y;
+                    if (value >= r_in * r_in && value <= r_out * r_out)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
