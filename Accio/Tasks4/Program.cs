1bd5ba52b7920ff291296c6266e89b6036e4e5e4
OnlyR0ck <guitarilu2019@gmail.com>
2020-10-11 14:45:32 +0300

    Add menu for Task4.

--- Accio/Tasks4/Program.cs
+++ Accio/Tasks4/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http.Headers;
+using System.Reflection.PortableExecutable;
+using System.Threading.Channels;
 
 namespace Tasks4
 {
@@ -6,7 +9,42 @@ namespace Tasks4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var code = 1;
+            while (Convert.ToBoolean(code))
+            {
+                Console.WriteLine("1 - Draw shapes\n2 - Draw flags\n3 - WTF\n0 - Exit");
+                Console.Write("Your choice : ");
+                int.TryParse(Console.ReadLine(), out code);
+                switch (code)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.Clear();
+            }
+        }
+    }
+
+    abstract class Shape
+    {
+        public abstract void Draw();
+    }
+
+    class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            throw new NotImplementedException();
         }
     }
 }
