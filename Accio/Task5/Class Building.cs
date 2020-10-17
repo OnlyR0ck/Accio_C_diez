0212071c120e1a7f9958ed412e27d49ceb6bcb51
smyglifreind <heits_rs001@mail.ru>
2020-10-17 22:38:38 +0300

    add task 2

---
+++ Accio/Task5/Class Building.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task5
+{
+    public class Building : IDisposable
+    {
+        public int Floors { get; set; }
+        public int Area { get; set; }
+        public int Occupents { get; set; }
+        public int AreaPerson()
+        {
+            return Area / Occupents;
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Disposed!!!");
+        }
+    }
+}
--- Accio/Task5/Program.cs
+++ Accio/Task5/Program.cs
@@ -9,17 +9,29 @@ namespace Task5
         {
             //Change array size if you want to add a function.
 
-            var delegates = new Menu.MenuDelegate[1];
+            var delegates = new Menu.MenuDelegate[2];
 
             //Explicitly initialize your function in an array.
 
             delegates[0] = Library;
+            delegates[1]= Using;
             Menu.StartMenu(delegates);
         }
 
         private static void Library()
         {
             Console.WriteLine("Hi, Do you like the menu?");
+        }
+        private static void Using()
+        {
+            int areapp;
+            using (Building house = new Building { Floors = 2, Area = 2500, Occupents = 4 })
+            {
+                areapp = house.AreaPerson();
+                Console.WriteLine($"The house has:\n {house.Floors} floors\n {house.Occupents} occupants {house.Area} square feet of total area and {areapp} for one person");
+
+            }
+            Console.WriteLine("The End :D");
         }
     }
 }
