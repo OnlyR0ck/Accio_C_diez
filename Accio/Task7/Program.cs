ca0e29cf1c7b241661d3eb77eb7e053118a43a62
OnlyR0ck <guitarilu2019@gmail.com>
2020-11-05 22:27:07 +0300

    Add competition Task7

---
+++ Accio/Task7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using HelperLibrary;
+using static Task7.MyThread;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var delegates = new Menu.MenuDelegate[] {Competition };
+            Menu.StartMenu(delegates);
+        }
+
+        static void Competition()
+        {
+            MyThread mt = new MyThread("97", 60);
+            MyThread mt2 = new MyThread("3", 60);
+        }
+    }
+}
