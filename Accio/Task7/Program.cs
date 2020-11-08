aa288e306c5bf12584e714d532889bdde94e3f03
Kirill <kirillpazitiv2020@gmail.com>
2020-11-08 17:47:05 +0300

    Rainbow gotova

--- Accio/Task7/Program.cs
+++ Accio/Task7/Program.cs
@@ -1,16 +1,42 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using HelperLibrary;
-using static Task7.MyThread;
+
 
 namespace Task7
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var delegates = new Menu.MenuDelegate[] {Competition };
+            var delegates = new Menu.MenuDelegate[] {Competition, SecondTask };
             Menu.StartMenu(delegates);
+        }
+
+        static void SecondTask()
+        {
+            Console.WriteLine("Enter country");
+            var country = Console.ReadLine();
+            CountryRainbow r = new CountryRainbow(country);
+            foreach (string s in r)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(s[0]);
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write(s[2]);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(s[3]);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(s[4]);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(s[5]);
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Write(s[6]);
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine(s[7]);
+            }
         }
 
         static void Competition()
@@ -19,4 +45,34 @@ namespace Task7
             MyThread mt2 = new MyThread("3", 60);
         }
     }
+
+    class CountryRainbow 
+    {
+  
+        private string[] rain = {"rainbow1", "rainbow2", "rainbow3", "rainbow4", "rainbow5" };
+        private readonly string _count;
+        public CountryRainbow(string country)
+        {
+            _count = country;
+        }
+        public IEnumerator<string> GetEnumerator()
+        {
+            if (_count != "Russia")
+            {
+                Console.WriteLine("Rainbow inaf");
+            }
+            else
+            {
+                    yield return rain[0];
+                    yield return rain[1];
+                    yield return rain[2];
+                    yield return rain[3];
+                    yield return rain[4];
+            }
+        }
+    }
+
+    
+
+    
 }
