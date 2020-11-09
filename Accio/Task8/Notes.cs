2858defac5896387564c2bd639461551364d8191
OnlyR0ck <guitarilu2019@gmail.com>
2020-11-09 13:30:47 +0300

    Finally, Task8.3 is ended.

---
+++ Accio/Task8/Notes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Notes : IAsyncDisposable
+    {
+        private int delay { get; set; }
+        public int frequency;
+        public int duration;
+
+        public Notes()
+        {
+            delay = 100;
+            frequency = 500;
+            duration = 100;
+        }
+
+        public Notes(int fr, int dur, int del)
+        {
+            this.frequency = fr;
+            this.duration = dur;
+            this.delay = del;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            Task.Delay(delay);
+            Console.Beep(frequency, duration);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Notes()
+        {
+            DisposeAsync();
+        }
+    }
+}
