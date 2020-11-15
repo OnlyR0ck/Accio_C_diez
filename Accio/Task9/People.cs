ef0b18ac59ddc1e6381883ae48bb9a8773400ed1
Kirill <kirillpazitiv2020@gmail.com>
2020-11-15 15:48:03 +0300

    Vrode Gotova

--- Accio/Task9/People.cs
+++ Accio/Task9/People.cs
@@ -1,18 +1,66 @@
-﻿namespace Task9
+﻿using System;
+
+namespace Task9
 {
     public  class People
     {
-        
+        public bool Liars;
+        public static int Lia = 0;
+        public static int Hon = 0;
+
+        public void Many()
+        {
+            Console.WriteLine($"In Game {Lia} Liars and {Hon} not a Liars");
+        }
+
+        public void Clear()
+        {
+            Lia = 0;
+            Hon = 0;
+        }
+
+
     }
 
     public class Liars : People
     {
+        public Liars()
+        {
+            Lia += 1;
+        }
+        public void PrevNext(bool liars)
+        {
+            if (liars)
+            {
+                Console.WriteLine(" man is not a liar");
+            }
+            else
+            {
+                Console.WriteLine(" man is a liar");
+            }
+        }
 
+     
     }
 
     public class Honest : People
     {
-
+        public Honest()
+        {
+            Hon += 1;
+        }
+        
+        public void PrevNext(bool liars)
+        {
+            if (liars)
+            {
+                Console.WriteLine(" man is a liar");
+            }
+            else
+            {
+                Console.WriteLine(" man is not a liar");
+            }
+        }
     }
 
 
--- Accio/Task9/Program.cs
+++ Accio/Task9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using HelperLibrary;
 
 namespace Task9
@@ -7,19 +8,53 @@ namespace Task9
     {
         static void Main(string[] args)
         {
-            var delegates = new Menu.MenuDelegate[] { };
+            var delegates = new Menu.MenuDelegate[] {Game };
             Menu.StartMenu(delegates);
         }
 
         static void Game()
         {
-
+            var n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            var people = Start(n);
+            Console.WriteLine();
+            people[i].Many();
+            Console.WriteLine();
+            Console.WriteLine("I am the first");
+            Console.Write("Next");
+            people[i].PrevNext(people[i + 1].Liars);
+            for (i = 1; i < n-1; i++)
+            {
+                Console.WriteLine($"{i} Man");
+                Console.Write("Pev");
+                people[i].PrevNext(people[i - 1].Liars);
+                Console.Write("Next");
+                people[i].PrevNext(people[i + 1].Liars);
+            }
+            Console.WriteLine("I am the last");
+            Console.Write("Pev");
+            people[i].PrevNext(people[i - 1].Liars);
+            people[i].Clear();
         }
 
-        static People Start(int n)
+        static List<dynamic> Start(int n)
         {
-            People* people = new People[n];
-            return people[n];
+            List<dynamic> people = new List<dynamic>();
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                if (rnd.Next(2) == 0)
+                {
+                    Console.WriteLine("Лгун");
+                    people.Add(new Liars() { Liars = true });
+                }
+                else
+                {
+                    Console.WriteLine("НеЛгун");
+                    people.Add(new Honest() { Liars = false });
+                }
+            }
+            return people;
         }
     }
 }
